976652521566d496a760331b729627a832511d6e
wismer <matthewhl@gmail.com>
2015-08-26 12:38:42 -0400

    try iron instead of hyper;

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,64 @@
-extern crate hyper;
-
-use std::fs::File;
-use std::io::Write;
+extern crate iron;
+use iron::prelude::*;
+use iron::{BeforeMiddleware, AfterMiddleware, typemap};
+use iron::status;
 use std::io::Read;
-use hyper::Server;
-use hyper::server::Request;
-use hyper::server::Response;
-use hyper::net::Fresh;
-use hyper::uri::RequestUri;
+use std::fs::File;
 
-fn hello(req: Request, res: Response<Fresh>) {
-    let Request { uri: uri, .. } = req;
-    let mut msg = String::new();
-    match uri {
-        RequestUri::AbsolutePath(path) => msg.push_str(&path),
-        RequestUri::AbsoluteUri(url) => println!("URL"),
-        RequestUri::Authority(val) => println!("{}", val),
-        RequestUri::Star => panic!("SDSKSDJFSDLKGJ")
-    }
-    let mut body: Vec<u8> = vec![];
-    let file = File::open("/Users/Matt/Downloads/AllSets.json");
-    let json = file.unwrap().read_to_end(&mut body);
-    res.send(&body).unwrap();
+// struct RoutePath<T> {
+//     slug: T,
+//     list: String
+// }
+
+struct Router;
+
+enum RouteTree {
+    Cards(i32),
+    Expansions(String)
 }
 
+impl Router {
+    fn new() -> Router {
+        Router
+    }
+
+    fn cards(&self, path: &Vec<String>, body: &mut String) {
+        let mut file = File::open("/Users/Matt/projects/ruby/hearthstone/data/AllSets.json").unwrap();
+        if path.len() == 1 {
+            file.read_to_string(body);
+        }
+    }
+
+    fn expansions(&self, path: &Vec<String>, body: &mut String) {
+        body.push_str("Expansions");
+    }
+
+    fn index(&self, body: &mut String) {
+        body.push_str("INDEX");
+    }
+
+    fn panic(&self) {
+        panic!("WHAT");
+    }
+}
+
+// request comes in. /cards/night-steed
+// break it apart -> "cards" "night-steed"
+// "cards" gets the type, so it will know that the following url part is a slug
+// the slug is used as a key in a hashmap that will then return the data as json
+
+
 fn main() {
-    Server::http("127.0.0.1:3000").unwrap().handle(hello);
+    Iron::new( |req: &mut Request| {
+        let path = &req.url.path.first().unwrap();
+        let router = Router::new();
+        let mut body = String::new();
+        match *path {
+            "cards"      => router.cards(&req.url.path, &mut body),
+            "expansions" => router.expansions(&req.url.path, &mut body),
+            ""           => router.index(&mut body),
+            _            => router.panic()
+        }
+        Ok(Response::with((status::Ok, body)))
+    }).http("localhost:3000").unwrap();
 }
